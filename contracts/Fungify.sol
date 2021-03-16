10f53ba106bed24dfffa097fe85dc5e7b9296f83
nginnever <ginneversource@gmail.com>
2021-03-16 15:11:04 -0700

    hardcode 6 nft ids

--- contracts/Fungify.sol
+++ contracts/Fungify.sol
@@ -10,13 +10,17 @@ contract Fraction is IFractional, ERC20 {
     address public owner;
     ERC721 public nft;
     
-    constructor (ERC721 _nft, uint _nftid, string memory _name, string memory _symbol, uint _total) ERC20(_name, _symbol) {
+    constructor (ERC721 _nft, uint[] memory _nftids, string memory _name, string memory _symbol, uint _total) ERC20(_name, _symbol) {
         require(_total > 1);
+        require(_nftids.length == 6);
         owner = msg.sender;
         nft = _nft;
         
-        _nft.transferFrom(msg.sender, address(this), _nftid);
-        require(_nft.ownerOf(_nftid) == address(this), "nft transfer failed");
+        for(uint i=0; i<6; i++) {
+            _nft.transferFrom(msg.sender, address(this), _nftids[i]);
+            require(_nft.ownerOf(_nftids[i]) == address(this), "nft transfer failed");
+        }
+
         _mint(msg.sender, _total * (10 ** uint256(decimals())));
         
         emit Fraction(address(nft), address(owner), _name, _symbol, _total);

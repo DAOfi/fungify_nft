053cec6e1dcb6f11d16628f2db13c8b61ec5c525
nginnever <ginneversource@gmail.com>
2021-03-17 17:20:36 -0700

    call script

---
+++ scripts/fungify.ts
@@ -0,0 +1,40 @@
+import { ethers } from 'ethers'
+import { deployContract } from 'ethereum-waffle'
+import Fraction from '../artifacts/contracts/Fraction.sol/Fraction.json'
+
+async function main() {
+  const provider = new ethers.providers.JsonRpcProvider(
+    process.env.JSONRPC_URL || 'https://kovan.poa.network'
+  )
+  const wallet = new ethers.Wallet(process.env.PRIVATE_KEY || '', provider)
+  // console.log('Wallet:', wallet.address)
+  let fraction = new ethers.Contract('0xFD43208897448dDf38ABEAe5BD1F8A2Be1B809da', Fraction.abi, provider)
+  let fractionSigner = fraction.connect(wallet)
+  let overrideOptions = {
+      gasLimit: 5000000,
+  }
+
+  let tokenIDs = [
+      ethers.BigNumber.from("1"),
+      ethers.BigNumber.from("2"),
+      ethers.BigNumber.from("3"),
+      ethers.BigNumber.from("4"),
+      ethers.BigNumber.from("5"),
+      ethers.BigNumber.from("6")
+  ]
+  let totalERC20 = ethers.BigNumber.from("888")
+
+  let fung = await fractionSigner.fungify(tokenIDs, totalERC20, overrideOptions)
+
+  provider.once(fung.hash, (receipt) => {
+      console.log('Transaction Minded: ' + receipt.transactionHash);
+      console.log(receipt);
+  })
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch(error => {
+    console.error(error)
+    process.exit(1)
+  });
